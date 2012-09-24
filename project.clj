1320c0d29d3c8729aeaead110d0ced7a898efbc8
Jim Crossley <jim@crossleys.org>
2012-09-24 16:43:55 -0400

    Add required deps for messaging outside of immutant

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.immutant/immutant-messaging "0.4.0-SNAPSHOT"]
+                 [org.hornetq/hornetq-core "2.2.18.Final"]
+                 [org.hornetq/hornetq-jms "2.2.18.Final"]
+                 [org.jboss.netty/netty "3.2.6.Final"]])
