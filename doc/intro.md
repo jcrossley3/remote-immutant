5e6ec79b5230079f1c36c263b73db5f518a75b45
Jim Crossley <jim@crossleys.org>
2012-09-24 16:01:03 -0400

    Initial project creation

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to remote
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/remote/core_test.clj
@@ -0,0 +1,7 @@
+(ns remote.core-test
+  (:use clojure.test
+        remote.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
